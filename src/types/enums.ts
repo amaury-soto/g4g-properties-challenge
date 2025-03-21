07d53dc661cf6514a2b8d781edf722a98fa77bea
amaury-soto <amaury.soto@globant.com>
2025-03-21 11:01:18 -0500

    challenge: add enums

---
+++ src/types/enums.ts
@@ -0,0 +1,15 @@
+export enum EBadgeIcon {
+  Agata = "agata",
+  Carraci = "carraci",
+}
+
+export enum EVariant {
+  Variant1 = "variant1",
+  Variant2 = "variant2",
+}
+
+export enum ETypes {
+  Header = "header",
+  Content = "content",
+  Footer = "footer",
+}
