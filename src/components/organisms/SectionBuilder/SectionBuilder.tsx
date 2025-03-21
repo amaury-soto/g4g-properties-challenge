7d42d57024d7183405deff5e08b9e86a84778241
amaury-soto <amaury.soto@globant.com>
2025-03-21 11:06:38 -0500

    challenge: add organisms - SectionBuilder component

---
+++ src/components/organisms/SectionBuilder/SectionBuilder.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import Section from "../../molecules/Section/Section";
+import Badge from "../../atoms/Badge/Badge";
+import Title from "../../atoms/Title/Title";
+import Subtitle from "../../atoms/Subtitle/Subtitle";
+import Card from "../../molecules/Card/Card";
+import styles from "./SectionBuilder.module.scss";
+import { EBadgeIcon, ETypes, EVariant } from "../../../types/enums";
+
+interface CardItem {
+  mediaSrc: string;
+  thumbnailSrc?: string;
+  isVideo: boolean;
+  title: string;
+  subtitle: string;
+}
+
+interface SectionBuilderProps {
+  variant: EVariant;
+  type: ETypes;
+  title?: string;
+  subtitle: string;
+  badgeIcon?: EBadgeIcon;
+  badgeText?: string;
+  badgeSubtitle?: string;
+  cardItems?: CardItem[];
+}
+
+const SectionBuilder: React.FC<SectionBuilderProps> = ({
+  variant,
+  type,
+  title,
+  subtitle,
+  badgeText,
+  badgeSubtitle,
+  cardItems = [],
+  badgeIcon,
+}) => {
+  const getContent = () => {
+    switch (type) {
+      case ETypes.Header:
+        return (
+          <div className={styles.headerContainer}>
+            <Title title={title!} variant={variant} />
+            <Subtitle subtitle={subtitle} variant={variant} />
+          </div>
+        );
+      case ETypes.Content:
+        return (
+          <div className={styles.headerContainer}>
+            <div
+              className={`${styles.titleBadgeContainer} ${
+                variant === EVariant.Variant2 ? styles.variant2 : ""
+              }`}
+            >
+              <div
+                className={`${styles.badgeContainer} ${
+                  variant === EVariant.Variant2 ? styles.variant2 : ""
+                }`}
+              >
+                <Badge
+                  badgeIcon={badgeIcon}
+                  text={badgeText || ""}
+                  variant={variant}
+                />
+                {variant === EVariant.Variant1 && badgeSubtitle && (
+                  <Subtitle subtitle={badgeSubtitle} variant={variant} as="p" />
+                )}
+              </div>
+              <Title title={title!} variant={variant} />
+            </div>
+            <Subtitle subtitle={subtitle} variant={variant} as="p" />
+            <div className={styles.mediaContainer}>
+              {cardItems.map((item, index) => (
+                <Card
+                  key={index}
+                  mediaSrc={item.mediaSrc}
+                  thumbnailSrc={item.thumbnailSrc}
+                  isVideo={item.isVideo}
+                  title={item.title}
+                  subtitle={item.subtitle}
+                  variant={variant}
+                />
+              ))}
+            </div>
+          </div>
+        );
+      case ETypes.Footer:
+        return (
+          <div className={styles.headerContainer}>
+            <Subtitle subtitle={subtitle} variant={variant} />
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return <Section variant={variant} content={getContent()} />;
+};
+
+export default SectionBuilder;
